acd7e95a6fd1f44462f4df3482d6d1dbdcc8d04f
Stephan Lukasczyk <stephan@lukasczyk.me>
2016-10-13 20:48:46 +0200

    Adjust screen layout for `alpspitze`

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP1 --mode 1920x1200 --pos 0x0 --rotate left --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 1200x720 --rotate normal --output VGA1 --off
+xrandr --output VIRTUAL1 --off --output DP1 --mode 1920x1200 --pos 0x0 --rotate left --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 1200x528 --rotate normal --output VGA1 --off
